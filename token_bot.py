d5c003e0c75a951e904ee04cc139bc05041a2b72
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-05-22 12:54:44 +0300

    delete token

--- token_bot.py
+++
@@ -1,7 +0,0 @@
-TOKEN = '802665822:AAFf93yEKOvuVUYIowsgJWbGFq2Ac2aIx24'
-
-key_map = 'AIzaSyCDD17I5tQ6UldB4irsTOj0dBahdZvSbe4'
-
-user_name = 'Katerina'
-passw = 'mishka2015'
-passw2 = 'morisday2019'
